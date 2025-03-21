e21175f23f2747d6136512bf1d2a7e0af373cfac
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-03-21 11:22:43 +0100

    import html-sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,49 @@ import "../scss/App.scss";
 
 function App() {
     return (
-        <h1>Hola mundo</h1>
-    )
+        <div>
+            <header>
+                <h1>¡Cuidado con Grogu!</h1>
+            </header>
+            <main class="page">
+                <section class="board">
+                    <div class="cell">
+                        <div class="grogu">👣</div>
+                    </div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                </section>
+
+                <section>
+                    <button class="dice">Lanzar Dado</button>
+                    <div class="game-status">En curso</div>
+                </section>
+
+                <section class="goods-container">
+                    <div class="goods-item">🍪</div>
+                    <div class="goods-item">🍪</div>
+                    <div class="goods-item">🍪</div>
+                </section>
+                <section class="goods-container">
+                    <div class="goods-item">🥚</div>
+                    <div class="goods-item">🥚</div>
+                    <div class="goods-item">🥚</div>
+                </section>
+                <section class="goods-container">
+                    <div class="goods-item">🐸</div>
+                    <div class="goods-item">🐸</div>
+                    <div class="goods-item">🐸</div>
+                </section>
+                <section>
+                    <button class="restart-button">Reiniciar Juego</button>
+                </section>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
